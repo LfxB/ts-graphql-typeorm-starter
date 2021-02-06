afcbaa64a983cbd16d03412fa62ede19aec7f4f2
LfxB <13532850+LfxB@users.noreply.github.com>
2021-02-06 16:54:59 -0500

    Add(client) subscriptions-transport-ws for graphql subscriptions

--- projects/client/src/graphql/client.ts
+++ projects/client/src/graphql/client.ts
@@ -1,7 +1,37 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER
+});
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER_WS || '',
+  options: {
+    reconnect: true
+  }
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const graphqlClient = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_SERVER,
+  link: splitLink,
   cache: new InMemoryCache()
 });
 
